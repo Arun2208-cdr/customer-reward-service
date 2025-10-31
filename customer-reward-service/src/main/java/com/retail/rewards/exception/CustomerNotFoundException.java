38dadf6f075d5151c2e006b12d39f103f8e5f595
Arun2208-cdr <arunjobs22@gmail.com>
2025-10-31 16:12:47 +0530

    Added exception handler to error normalize messages

---
+++ customer-reward-service/src/main/java/com/retail/rewards/exception/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.retail.rewards.exception;
+
+public class CustomerNotFoundException extends Exception{
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
