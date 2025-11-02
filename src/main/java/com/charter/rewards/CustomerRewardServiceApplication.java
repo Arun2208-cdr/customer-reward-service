971e87f0f550188e0c8c9db86808c44a11c19172
Arun2208-cdr <arunjobs22@gmail.com>
2025-11-02 23:08:49 +0530

    add main Spring Boot application entry point

---
+++ src/main/java/com/charter/rewards/CustomerRewardServiceApplication.java
@@ -0,0 +1,11 @@
+package com.charter.rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRewardServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerRewardServiceApplication.class, args);
+    }
+}
