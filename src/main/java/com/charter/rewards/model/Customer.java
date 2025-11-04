e7995eb925e7a74bd58bcd8650498eb4e1ad3322
Arun2208-cdr <arunjobs22@gmail.com>
2025-11-04 21:47:29 +0530

    removed unused imports

--- src/main/java/com/charter/rewards/model/Customer.java
+++ src/main/java/com/charter/rewards/model/Customer.java
@@ -3,8 +3,6 @@ package com.charter.rewards.model;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
